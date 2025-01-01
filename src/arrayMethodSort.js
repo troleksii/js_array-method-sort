604671ffd6129d1f176c64f621ebc74e6753e172
troleksii <aliosha123418@gmail.com>
2025-01-01 21:19:19 +0100

    Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -5,7 +5,32 @@
  */
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+    const arr = this;
+
+    if(!compareFunction) {
+      compareFunction = (a, b) => {
+        const currentValue = a.toString();
+        const nextValue = b.toString();
+        if (currentValue > nextValue) {
+          return 1;
+        } else if (currentValue < nextValue) {
+          return -1;
+        } else {
+          return 0
+        }
+      }
+    }
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = 0; j < arr.length - 1; j++) {
+        if (compareFunction(arr[j], arr[j + 1]) > 0) {
+          // Обмін місцями елементів
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+
+    return arr
   };
 }
 
